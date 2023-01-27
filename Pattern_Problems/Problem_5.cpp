f819470f25c4e90d5e9b2f99ce5670a81b0da614
LOVKUSH <lovkushyadav9888@gmail.com>
2023-01-27 07:27:43 +0530

    Pattern

---
+++ Pattern_Problems/Problem_5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0; i<=n; i++){
+        for(int j=0; j>=n; j--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+
+}
