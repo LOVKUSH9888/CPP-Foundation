868bae4a610967ed89344c53987d9ac2e0b3e523
LOVKUSH <lovkushyadav9888@gmail.com>
2023-01-29 02:26:57 +0530

    Another method of pro_7.cpp

--- Pattern_Problems/Problem_7.cpp
+++ Pattern_Problems/Problem_7.cpp
@@ -12,3 +12,20 @@ int main (){
     }
     return 0;
 }
+
+
+//Another way of doing this is:
+int main(){
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n-i; j++){
+            cout<<" ";
+        }
+        for(int j = 0; j<2*i+1; j++){
+            cout<<"*";
+        }
+        for(int j = 0; j<n-i-1; j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
