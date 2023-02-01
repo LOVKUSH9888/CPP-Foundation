341b5c3c4d8d23dece7c605271d6ac70f8128230
LOVKUSH <lovkushyadav9888@gmail.com>
2023-02-01 17:36:26 +0530

    Power using Recursion

---
+++ Recursion_Basics/4th_Power.cpp
@@ -0,0 +1,30 @@
+// x^n = 0, 1, 2 ................
+// 5^3 = 5 * 5 * 5 = 125
+//Base case = n = 0 the x^0 = 1 & also we can take 1
+// Lets think about Recursive Case = x*x^(n-1)
+// Recursive case = pow(x, n) = pow(x, n-1) * x
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int power(int x, int n){
+    //Base Case
+    if(n==0) return 1;
+
+    //Recursive Case
+    int smallOutput = power(x, n-1);
+
+    //#rd step calculation
+    return x*smallOutput;
+}
+
+
+int main (){
+    cout<<power(5, 3);
+    return 0;
+
+}
+
+
+//Output = 5 * 3 = 123 ans
