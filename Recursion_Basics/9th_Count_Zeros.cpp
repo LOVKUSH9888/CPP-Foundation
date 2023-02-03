a494ba8d5b70262cb6754d74eb54bdafc0bd50b6
LOVKUSH <lovkushyadav9888@gmail.com>
2023-02-03 08:38:31 +0530

    Count_Zeros

---
+++ Recursion_Basics/9th_Count_Zeros.cpp
@@ -0,0 +1,36 @@
+//Given no. = 10320 = 0+0 = 2 zeros
+//If n = 123 = then 0 count = 0
+
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int countZeros(int n){
+    //BaseCase
+    if(n==0){
+        return 0;
+    }
+
+    //recursive CAse
+
+    int smallAns = countZeros(n/10);   //Smallest Value 
+
+    //Calculation
+
+    int last_digit = n % 10; 
+
+    if(last_digit == 0){
+        return 1 + smallAns;
+    }else {
+        return smallAns;
+    }
+    
+}
+
+int main (){
+    cout<<countZeros(10230);
+
+}
+
+
+//Output = 2ans
