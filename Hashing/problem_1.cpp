f3471a710f339769ea6989687b7f3a708a1fddd5
LOVKUSH <lovkushyadav9888@gmail.com>
2023-02-15 07:34:08 +0530

    Solving 1 basic problem

---
+++ Hashing/problem_1.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+
+// for e.g. array = {2, 2, 2, 1, 1, 4, 3, 4};
+///Here key(elements) = 2, 1, 4, 3 
+///Value(frequency) = 3, 2, 2, 1 {how many time the elemets comes}
+
+
+int main(){
+
+     //Declaration & taking input
+
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[];
+    }
+
+    //Taking Queries from the user
+
+    int q;
+    cin>>q;
+    while(q--){
+        int number;
+        cin>>number;
+    }
+
+
+}
+
