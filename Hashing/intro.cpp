8d5a7cdd16034795c35eb4b7eb042abe33ffa3c7
LOVKUSH <lovkushyadav9888@gmail.com>
2023-03-09 15:21:40 +0530

    Hash-MAp-Theory completion

--- Hashing/intro.cpp
+++ Hashing/intro.cpp
@@ -5,7 +5,7 @@
 // Hash Table = Data Structure 
 // Hash Function = k mod 10 ===== 24/10 = 4 is remainder so we will store 24 at 4th index
 
-///AT the point of collision we used two techniques -1. Chaining(Open Hashing) && 2. Open Addressing(Closed Hashing)
+///AT the point of collision we used two techniques -1. Chaining(Open Hashing) && 2. Open Addressing (Closed Hashing)
 ///In chaining = We make Linked List and add the data on same index by making chaining
 
 
@@ -17,4 +17,68 @@
 
 /////#implementaion of hashmap: Insert-delete-search
 //1. Linked-List
-//2. Balanced BST = This will better than Linked LIst
+//2. Balanced BST = This will better than Linked LIst.
+
+
+
+//3.3.2023
+
+/*iN -bUILT STL*/
+//1.MAP - O(logN)
+//2.Un-Ordered-MAP = O(1)
+
+
+//Creating HashMap:-
+#include <iostream>
+#include<map>
+#include<unordered_map>
+using namespace std;
+
+int main()
+{
+    //Map-Creation
+    unordered_map<int, int> m;  //When key = int & value = int
+    unordered_map<string, int> m2; //WHen key = string & value = int
+
+    //Insertion-Method:-
+    //1st make pair
+    pair<string, int> p = make_pair("Love", 3);
+    //Insert
+    m2.insert(p);
+
+    //Another method of insertion :-)
+    m["Kush"] = 1;
+
+
+    //Searching-Method::-
+    cout<<m2["Love"]<<endl; //This methof will create a new entry by putting 0
+    //2nd-way
+    cout<<m2.at("Love")<<endl;  //use by at.
+
+    cout<<m2.at("unknown_key")<<endl; //This will give uncaught error
+
+
+    //Checking - Map - Size
+    cout<<m2.size()<<endl;
+
+
+    //To-Check Presence
+    cout<<m2.count("bro")<<endl; //in this it will give 0 as its not present
+    
+
+    //Erase
+    m2.erase("Love");
+    cout<<m.size()<<endl;
+
+
+    //Accessing elment = We use Iterators
+    unordered_map<string, int>::iterator it = m2.begin();
+
+    //now use while loop to print
+    while(it != m2.end()){
+        cout<<it->first<<it->second<<endl;
+        it++;
+    }
+
+    return 0;
+}
