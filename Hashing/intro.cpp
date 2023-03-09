98b00595d3e0c6f01586f6bc7d8deba255a195ef
LOVKUSH <lovkushyadav9888@gmail.com>
2023-03-09 18:20:05 +0530

    hashmap complete concept

--- Hashing/intro.cpp
+++ Hashing/intro.cpp
@@ -1,6 +1,7 @@
 //It is similar to array  {it has indexes but Diff is we dont need to scan entire data for finding the data}
 //It is used to store & retrieve data in O(1) time == iNSERTION, DELETITION & SEARCHING
 //It is alo called mapping technique
+///This can be used in multiple problems such as - ARRAY, LINKED LIST, GRAPH & DP
 //# imp = Search Key +  Hash Table + Hash Function
 // Hash Table = Data Structure 
 // Hash Function = k mod 10 ===== 24/10 = 4 is remainder so we will store 24 at 4th index
@@ -64,7 +65,7 @@ int main()
 
     //To-Check Presence
     cout<<m2.count("bro")<<endl; //in this it will give 0 as its not present
-    
+
 
     //Erase
     m2.erase("Love");
@@ -82,3 +83,73 @@ int main()
 
     return 0;
 }
+
+
+
+//LEETCODE 1838 : Frequency of the Most Frequent Element
+
+
+///HASH_FUNCTION = Hash Code + Comparison Function
+
+
+
+///Collision Handling -> 1. Open Hashing/Open Chaining = Put at the same index (Linked-List using Head)
+//2. Closed Hashing/Open Addressing = 
+
+
+//Revision:-
+#include<iostream>
+#include<unordered_map>
+#include<string>
+using namespace std;
+
+int main(){
+    unordered_map<string, int>  mymap;
+
+    /// insert
+    pair<string,int> p("abc",1);
+    mymap.insert(p);
+
+    mymap["def"] = 2;  /// like array
+
+    /// find or access
+     cout<<mymap["abc"]<<endl;
+     cout<<mymap.at("abc")<<endl;
+
+     cout<<"Size "<<mymap.size()<<endl;
+
+     /// cout<<mymap.at("ghi")<<endl;   /// throw exception
+     cout<<mymap["ghi"]<<endl;
+
+    cout<<"Size "<<mymap.size()<<endl;
+
+
+     /// check presence of a key
+     if(mymap.count("ghi") > 0){
+        cout<<"ghi is present"<<endl;
+     }else{
+        cout<<"not present"<<endl;
+     }
+
+
+     /// update
+     mymap["abc"] = 20;
+
+     /// erase
+     mymap.erase("ghi");
+
+     cout<<"Size "<<mymap.size()<<endl;
+
+
+     /// check presence of a key
+     if(mymap.count("ghi") > 0){
+        cout<<"ghi is present"<<endl;
+     }else{
+        cout<<"not present"<<endl;
+     }
+
+    return 0;
+}
+
+
+
