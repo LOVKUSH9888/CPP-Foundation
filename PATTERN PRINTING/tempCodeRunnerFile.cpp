43e5b34138810980feaec973dbb61228c72981fc
LOVKUSH <lovkushyadav9888@gmail.com>
2023-04-22 17:21:06 +0530

    Pattern revision

---
+++ PATTERN PRINTING/tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+//https://www.programiz.com/cpp-programming/examples/pyramid-pattern
+
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=0; i<=5; i++){
+        for(int j = 0; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
--- Pattern_Problems/tempCodeRunnerFile.cpp
+++ Pattern_Problems/tempCodeRunnerFile.cpp
@@ -5,7 +5,7 @@ int main(){
     cout<<"Enter the Number of Rows";
     cin>>n;
     for(int i=0; i<=n; i++){   //Rows
-        for(int j = 0; j<=n; j++){   //Columns
+        for(int j <= 0; j<=n; j++){   //Columns
             cout<<"* ";    //* always is in this inside loop
         }
         cout<<endl;    // For next line moving 
